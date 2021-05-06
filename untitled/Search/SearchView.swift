ea408ef3c3dbffae5991bd384caf49b87b1b6299
ddevilore <amnmbrone@yahoo.com>
2021-05-06 23:23:24 +0400

    Project init

---
+++ untitled/Search/SearchView.swift
@@ -0,0 +1,14 @@
+//
+//  SearchView.swift
+//  untitled
+//
+//  Created by Artem on 06.05.2021.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search hello")
+    }
+}
---
+++ untitled/Shared/Enums.swift
@@ -0,0 +1,12 @@
+//
+//  TabEnums.swift
+//  untitled
+//
+//  Created by Artem on 06.05.2021.
+//
+
+enum tabs {
+    case dataList
+    case search
+    case profile
+}
